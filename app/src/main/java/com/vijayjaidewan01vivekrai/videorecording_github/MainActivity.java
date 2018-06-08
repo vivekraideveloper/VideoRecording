4d9036ef67b8eccfb79f2973036e27739a0f5d79
Vivek Rai <vijayjaidewan01@gmail.com>
2018-06-08 16:14:10 +0530

    Initial commit

---
+++ app/src/main/java/com/vijayjaidewan01vivekrai/videorecording_github/MainActivity.java
@@ -0,0 +1,29 @@
+package com.vijayjaidewan01vivekrai.videorecording_github;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivity(intent);
+            }
+        });
+    }
+}
